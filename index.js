e2295294eb77f5880bac40fdc5817eb1af1c4a53
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-09 21:05:18 -0800

    Added functionality to draw new cards & get info

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
+
+function drawCards() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckID}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.cards.length; i++) {
+        console.log(
+          `Card #${i + 1}: ${data.cards[i].value} of ${data.cards[i].suit}`
+        );
+      }
+      console.log(data);
+    });
+}
+
+document.getElementById("draw-cards").addEventListener("click", drawCards);
